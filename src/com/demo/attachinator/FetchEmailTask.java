c6561c663a56616d45905a25496261a436dfe253
sthaviso <sutthipong@gmail.com>
2014-11-06 15:44:27 -0800

    Initial check in for gmail api calling

---
+++ src/com/demo/attachinator/FetchEmailTask.java
@@ -0,0 +1,113 @@
+package com.demo.attachinator;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.json.JSONException;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import com.google.android.gms.auth.GoogleAuthException;
+import com.google.android.gms.auth.GoogleAuthUtil;
+import com.google.android.gms.auth.UserRecoverableAuthException;
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.services.gmail.Gmail;
+import com.google.api.services.gmail.model.ListThreadsResponse;
+
+public class FetchEmailTask extends AsyncTask<Void, Void, Void> {
+
+	private String mScope;
+	private MainActivity mActivity;
+	private String mEmail;
+
+	private static final String TAG = "TokenInfoTask";
+	private static final String APP_NAME = "Attachinator";
+
+	public FetchEmailTask(MainActivity activity, String email, String scope) {
+		mActivity = activity;
+		mEmail = email;
+		mScope = scope;
+	}
+
+	@Override
+	protected Void doInBackground(Void... params) {
+		try {
+			fetchEmailFromGoogle();
+		} catch (IOException ex) {
+			onError("Following Error occured, please try again. "
+					+ ex.getMessage(), ex);
+		} catch (JSONException e) {
+			onError("Bad response: " + e.getMessage(), e);
+		}
+		return null;
+	}
+
+	/**
+	 * Get a authentication token if one is not available. If the error is not
+	 * recoverable then it displays the error message on parent activity right
+	 * away.
+	 */
+	protected String fetchToken() throws IOException {
+		try {
+			return GoogleAuthUtil.getToken(mActivity, mEmail, mScope);
+		} catch (UserRecoverableAuthException userRecoverableException) {
+			// GooglePlayServices.apk is either old, disabled, or not present,
+			// which is
+			// recoverable, so we need to show the user some UI through the
+			// activity.
+			mActivity.handleException(userRecoverableException);
+		} catch (GoogleAuthException fatalException) {
+			onError("Unrecoverable error " + fatalException.getMessage(),
+					fatalException);
+		}
+		return null;
+	}
+
+	protected void onError(String msg, Exception e) {
+		if (e != null) {
+			Log.e(TAG, "Exception: ", e);
+		}
+		mActivity.show(msg); // will be run in UI thread
+	}
+
+	private void fetchEmailFromGoogle() throws IOException, JSONException {
+		String token = fetchToken();
+		if (token == null) {
+			// error has already been handled in fetchToken()
+			return;
+		}
+
+		HttpTransport httpTransport = new NetHttpTransport();
+		JsonFactory jsonFactory = new JacksonFactory();
+		GoogleCredential credential = new GoogleCredential()
+				.setAccessToken(token);
+
+		// Create a new authorized Gmail API client
+
+		Gmail service = new Gmail.Builder(httpTransport, jsonFactory,
+				credential).setApplicationName(APP_NAME).build();
+
+		// Retrieve a page of Threads; max of 100 by default.
+
+		ListThreadsResponse threadsResponse = service.users().threads()
+				.list(mEmail).execute();
+
+		List<com.google.api.services.gmail.model.Thread> threads = threadsResponse
+				.getThreads();
+
+		// Print ID of each Thread.
+
+		for (com.google.api.services.gmail.model.Thread thread : threads) {
+
+			System.out.println("Thread ID: " + thread.getId());
+
+		}
+
+	}
+
+}
